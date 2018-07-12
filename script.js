ad23ecc05e60029c26cfcf4dde050b6102e67ada
Ярослав <mulo-_-2@mail.ru>
2018-07-12 23:35:46 +0300

    New design

--- script.js
+++ script.js
@@ -2,22 +2,60 @@ var seconds=document.getElementsByClassName("seconds");
 var minutes=document.getElementsByClassName("minutes");
 var currMinute=0;
 var currSec=0
-var timerID=setInterval(function timer(){
+var started=true;
+var timerID=setInterval("timer()",1000);
+  function timer() {
     currSec+=1;
-  if(currSec==60)
-  {
-    minutes[0].textContent=currMinute+=1;
-    seconds[0].textContent=0;
-    currSec=0;
+    console.log(currSec);
+    if(currSec==60)
+    {
+      if(currMinute<=9){
+        minutes[0].textContent="0"+currMinute;
+      }
+      else {
+        minutes[0].textContent=currMinute+=1;
+      }
+      seconds[0].textContent=0;
+      currSec=0;
+    }
+    else if(currMinute==59&&currSec==60)
+    {
+      currSec=0;
+      currMinute=0;
+      seconds[0].textContent="0"+0;
+      minutes[0].textContent="0"+0;
+    }
+    else {
+      if(currSec<=9){
+        seconds[0].textContent="0"+currSec;
+      }
+      else {
+        seconds[0].textContent=currSec;
+      }
+    }
   }
-  else if(currMinute==59&&currSec==60)
+function reset() {
+  console.log("adas");
+  currMinute=0;
+  currSec=0
+  seconds[0].textContent="0"+0;
+  minutes[0].textContent="0"+0;
+}
+function pause()
+{
+  if(started==true)
   {
-    currSec=0;
-    currMinute=0;
-    seconds[0].textContent=0;
-    minutes[0].textContent=0;
+    clearInterval(timerID);
+    started=false
   }
   else {
-      seconds[0].textContent=currSec;
+    timerID=setInterval("timer()",1000);
+    started=true;
   }
-},1000);
+}
+$("#reset").click(function(){
+  reset();
+});
+$("#start").click(function () {
+  pause();
+})
